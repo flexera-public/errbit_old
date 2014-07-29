1f56d7021e08eb85b390dfef4848687b9c2e52f0
Tony Spataro <tony@rightscale.com>
2014-07-28 17:03:04 -0700

    Add new simplistic fingerprint

---
+++ app/models/fingerprints/simplistic_fingerprint.rb
@@ -0,0 +1,22 @@
+require 'digest/md5'
+
+# A deliberately simplistic fingerprint that considers ONLY the error class and top of the backtrace
+# (i.e. the line on which the exception was raised). This is useful for applications that generate
+# a large volume of errors that have different messages or backtraces
+class SimplisticFingerprint < Fingerprint
+  def fingerprint_source
+    {
+      :location        => location,
+      :error_class     => notice.error_class,
+    }
+  end
+
+  private
+  def sanitized_method_signature
+    location['method'].gsub(/[0-9]+|FRAGMENT/, '#').gsub(/_+#/, '_#')
+  end
+
+  def location
+    notice.backtrace.lines.first
+  end
+end
